40c7e87dd7659386bc08f6cfe6d1a3f356516276
Andrzej Szarata <aszarata@puapka.local>
2025-12-30 18:01:43 +0100

    Minor fix

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,9 +1,11 @@
+import os
 from src.scripts.download_artifacts import download_from_s3
 from src.scripts.export_classifier_to_onnx import export_classifier_to_onnx
 from src.scripts.export_sentence_transformer_to_onnx import export_model_to_onnx
 from src.scripts.settings import Settings
 
 if __name__ == "__main__":
+    os.makedirs("model", exist_ok=True)
     settings = Settings()
     print("Downloading model")
     download_from_s3(settings)
